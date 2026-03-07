db2c3ac0836618232b9551d63bdc7cd321b13174
ck <liangyu456@gmail.com>
2026-03-07 10:32:14 -0800

    update

---
+++ app/src/main/java/com/example/myapplication/features/doordash/module/navigation/DoorDashNavEntries.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication.features.doordash.module.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation3.runtime.EntryProviderScope
+import androidx.navigation3.runtime.NavKey
+import com.example.core.navigation.module.Navigator
+import com.example.myapplication.R
+import com.example.myapplication.module.navigation.Route
+
+@Composable
+fun EntryProviderScope<NavKey>.DoorDashNavEntries(
+    navigator: Navigator
+) {
+    entry<Route.DoorDash> {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = stringResource(R.string.title_doordash))
+        }
+    }
+}
